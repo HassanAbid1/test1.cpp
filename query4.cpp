fcc1ef4d52ed85f8a051a2d9303d9ef568482525
HassanAbid1 <hassanabidofficial@gmail.com>
2024-03-21 02:59:45 +0500

    even numbers print

--- query4.cpp
+++ query4.cpp
@@ -2,21 +2,8 @@
 using namespace std;
 
 int main(){
-    // initializing the array
-    int numbers[3];
-    //taking user input
-    cout<<"Enter 3 numbers: "<<endl;
-
-    //using forloop for storing the numbers in array
-    for(int i=0; i<3; i++){
-        cin>>numbers[i];
+    //using forloop to print even numbers between 1 to 100
+     for (int i = 2; i <= 100; i += 2) {
+        cout << i << " ";
     }
-    //using forloop to print even numbers
-    for(int i=0; i<3; i+=2){
-        if(numbers[i]%2==0){
-            cout<<numbers[i]<<" ";
-        }
-    }
-    //else statement
-    cout<<"No even numbers";
 }
