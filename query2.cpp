95ff77fbbaa39dff527f44c544f3af6a41214104
HassanAbid1 <hassanabidofficial@gmail.com>
2024-03-21 01:49:16 +0500

    sum changes

--- query2.cpp
+++ query2.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int numbers[]={3, 4, 5, 6, 8, 7};
+    int numbers[6]={6, 4, 5, 6, 8, 7};
     int sum=0;
-     int size=sizeof(numbers)/sizeof(numbers[0]);
-     for (int i=0; i<size; i++){
+     
+     for (int i=0; i<6; i++){
         sum = sum+numbers[i];
      }
      cout<<"The sum of all numbers present in array is: "<<sum<<endl;
