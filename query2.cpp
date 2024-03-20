41df8ce015eaeb363b4f519d140b883ba88bd79c
HassanAbid1 <hassanabidofficial@gmail.com>
2024-03-21 02:20:37 +0500

    sum of elements

--- query2.cpp
+++ query2.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
+   // initializing array
     int numbers[6]={6, 4, 5, 6, 8, 7};
+
+    // declaring a variable for storing sum of all numbers
     int sum=0;
-     
+
+     // using for loop 
      for (int i=0; i<6; i++){
         sum = sum+numbers[i];
      }
+
+     // result
      cout<<"The sum of all numbers present in array is: "<<sum<<endl;
 }
--- query3.cpp
+++ query3.cpp
@@ -2,19 +2,28 @@
 using namespace std;
 
 int main(){
+    // initializing the array
     int numbers[6];
     cout<<"Enter 6 numbers: "<<endl;
+   
+    // declaring the varibale for storing max number from array
     int max=0;
+
+    //using while loop for storing values in array
     int j=0;
     while(j<6){
         cin>>numbers[j];
         j++;
     }
+
+    //using for loop to find max number
    for(int i=1; i<6; i++){
+    //conditional statement for max number
     if(numbers[i]>max){
         max=numbers[i];
     }
    }
 
+// result
    cout<<"Greater number is: "<<max<<endl;
 }
