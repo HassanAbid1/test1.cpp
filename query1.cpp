26936b82952e1c5c10200606da2ad7b0902b0ff5
HassanAbid1 <hassanabidofficial@gmail.com>
2024-03-21 02:12:44 +0500

    reverse

--- query1.cpp
+++ query1.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main(){
 //initializing an array
-int numbers[10];
+int numbers[12];
    // for saving 10 numbers in array using for loop
-    cout<<"Enter 10 numbers: "<<endl;
-    for(int i=0; i<10; i++){
+    cout<<"Enter 12 numbers: "<<endl;
+    for(int i=0; i<12; i++){
         cin>>numbers[i];
     }
 
   // for printing reverse order
   cout<<"Reverse order is:"<<endl;
-    for(int i=10-1; i>=0; i--){
+    for(int i=12-1; i>=0; i--){
         cout<<numbers[i]<<endl;
     }
 }
