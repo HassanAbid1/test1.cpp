1b23967b7e52ac07a82556622622232a7dc93ec9
HassanAbid1 <hassanabidofficial@gmail.com>
2024-03-21 02:24:10 +0500

    max number

--- query3.cpp
+++ query3.cpp
@@ -3,21 +3,21 @@ using namespace std;
 
 int main(){
     // initializing the array
-    int numbers[6];
-    cout<<"Enter 6 numbers: "<<endl;
+    int numbers[10];
+    cout<<"Enter 10 numbers: "<<endl;
    
     // declaring the varibale for storing max number from array
     int max=0;
 
     //using while loop for storing values in array
     int j=0;
-    while(j<6){
+    while(j<10){
         cin>>numbers[j];
         j++;
     }
 
     //using for loop to find max number
-   for(int i=1; i<6; i++){
+   for(int i=1; i<10; i++){
     //conditional statement for max number
     if(numbers[i]>max){
         max=numbers[i];
